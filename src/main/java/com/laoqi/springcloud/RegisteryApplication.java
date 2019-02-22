a9fc39e139686975737e32cb1fb05dc8b2298592
jason <zengjin_liu@aliyun.com>
2019-02-22 10:06:39 +0800

    Eureka server服务端

---
+++ src/main/java/com/laoqi/springcloud/RegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.laoqi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisteryApplication.class, args);
+	}
+
+}
